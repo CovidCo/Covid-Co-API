26ca1325d5085c9146859d27f5a8b1f15cc8b65f
Samara Enriquez <samara.enriquez@thebitbang.company>
2020-03-23 18:36:43 -0500

    Servicio para envio de mensajes creado

---
+++ functions/utils/sms_manager.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+let deliverMessage = async (phoneNumber) => {
+    var params = {
+        Message: 'Hola, nuevo registro',
+        PhoneNumber: '+' + phoneNumber.toString()
+    };
+    var messageAttributes = {
+        attributes: {
+            "DefaultSMSType": "Transactional"
+        }
+    }
+    // code credit AWS team
+    // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sns-examples-publishing-messages.html
+    try {
+        let publishTextMessagePromise = new AWS.SNS({apiVersion: '2010-03-31' , 
+        											accessKeyId: 'accessKeyId',
+        											secretAccessKey: 'secretAccessKey',
+        											region: 'us-east-1' })
+        await publishTextMessagePromise.setSMSAttributes(messageAttributes).promise()
+        let messagePromise = publishTextMessagePromise.publish(params).promise();
+        return {error:false, messagePromise}
+    } catch (e) {
+        console.log(e)
+        return {error: true, e}
+    }
+}
+
+module.exports = {
+  deliverMessage
+}
