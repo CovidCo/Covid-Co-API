b320bc47357480a9b304210e40fe13fa985445e6
Esteban Ceron <estebance@Estebans-MacBook-Air.local>
2020-03-22 19:37:49 -0500

    [FEATURE] close connection

--- functions/utils/db_manager.js
+++ functions/utils/db_manager.js
@@ -9,38 +9,37 @@ class DbManager {
         password: functions.config().db.password,
         database: functions.config().db.db_name,
       })
-      this.dbClient.connect()
     }
 
     executeTransaction(sqlStatement, queryPayload){
       return new Promise((resolve, reject) => {
+        this.dbClient.connect()
         this.dbClient.query(sqlStatement, queryPayload).then((res) => {
-          console.log(res)
-          resolve(res)
-        }).catch((e) => {
-          console.error(e.stack)
-          reject(e)
-        })
-        console.log('close connection')
-        this.dbClient.end()
-      })
-    }
-
-    executeQuery(sqlQuery){
-      return new Promise((resolve, reject) => {
-        this.dbClient.query(sqlQuery).then((res) => {
           console.log(res)
           this.dbClient.end()
           resolve(res)
         }).catch((e) => {
           console.error(e.stack)
+          this.dbClient.end()
           reject(e)
         })
-        console.log('close connection')
-        this.dbClient.end()
       })
     }
 
+    executeQuery(sqlQuery){
+      return new Promise((resolve, reject) => {
+        this.dbClient.connect()
+        this.dbClient.query(sqlQuery).then((res) => {
+          console.log(res)
+          this.dbClient.end()
+          resolve(res)
+        }).catch((e) => {
+          this.dbClient.end()
+          console.error(e.stack)
+          reject(e)
+        })
+      })
+    }
 }
 
 module.exports = DbManager
